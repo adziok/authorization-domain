af6923e41a25e3f041b7ac055908cd42f4b9bf08
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-05-11 21:47:46 +0200

    refactor: authorization factory use Authorization constructor

--- src/modules/authorization/domain/factories/AuthorizationFactory.ts
+++ src/modules/authorization/domain/factories/AuthorizationFactory.ts
@@ -1,9 +1,8 @@
 import { EmailVO } from '../value-objects/EmailVO';
-import { PasswordVO } from '../value-objects/HashedPasswordVO';
+import { PasswordVO } from '../value-objects/PasswordVO';
 import { IHashingService } from '../services/IHashingService';
 import { IConfirmationTokenService } from '../services/IConfirmationTokenService';
 import { Authorization } from '../Authorization';
-
 
 export class AuthorizationFactory {
     constructor(private hashingService: IHashingService, private confirmationTokenService: IConfirmationTokenService) { }
@@ -11,7 +10,7 @@ export class AuthorizationFactory {
     async create(email: EmailVO, password: PasswordVO) {
         const hashedPassword = await this.hashingService.hashPassword(password);
         const confirmationToken = await this.confirmationTokenService.generate();
-        const authorization = Authorization.create(email, hashedPassword, confirmationToken);
+        const authorization = new Authorization({ email, password: hashedPassword, confirmationToken });
 
         return authorization;
     }
