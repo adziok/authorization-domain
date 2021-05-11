7164b3a8620bcd556e301181b66e6d21246aceb4
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-05-11 21:48:33 +0200

    fix: change email validation

--- src/modules/authorization/domain/value-objects/EmailVO.ts
+++ src/modules/authorization/domain/value-objects/EmailVO.ts
@@ -1,14 +1,18 @@
 import { ValueObject } from '../../../../shared/interfaces/ValueObject';
 import { InvalidEmailException } from '../exceptions/InvalidEmailException';
 
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function validateEmail(email: string): boolean {
+    return emailRegex.test(email.toLowerCase());
+}
 
 export class EmailVO extends ValueObject<EmailVO> {
     private email: string;
 
     constructor(email: string) {
         super();
-        if (!email.match(emailRegex)) {
+        if (!validateEmail(email)) {
             throw new InvalidEmailException();
         }
         this.email = email;
