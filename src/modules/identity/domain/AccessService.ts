e6d5b100fb8cf9d80916d5a97ecf80413f09d30b
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-05-16 16:41:24 +0200

    add: is access object exists in database check in access service

--- src/modules/identity/domain/AccessService.ts
+++ src/modules/identity/domain/AccessService.ts
@@ -1,12 +1,14 @@
 import { AccessRepository } from "./AccessRepository";
 import { Identity } from './Identity';
 
-
 export class AccessService {
     constructor(private accessRepository: AccessRepository) { }
 
     async hasAccess(identity: Identity, externalId: string, resourceName: string, action: string): Promise<boolean> {
         const access = await this.accessRepository.getAccessByIdentityIdAndExternalId(identity.id, externalId);
+        if (!access) {
+            throw new Error('Invalid external id or missing permissions')
+        }
         return access.hasAccess(`${resourceName}:${action}`);
     }
 }
